371c1b52aba4b62975734d0b80166e82d17cbc26
Conor Dockry <conordockry@gmail.com>
2018-06-07 12:00:16 -0400

    remove config

--- src/lib/firebase.tsx
+++ src/lib/firebase.tsx
@@ -1,7 +1,6 @@
 import firebase, { firestore, app } from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
-// import config from './firebase-config'
 
 const config: any = {}
 config.apiKey = process.env.REACT_APP_FIREBASE_API_KEY!
